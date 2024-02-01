1680bcea0357ff52bf15fb00faa5e96e9b50cc6c
Bodin Hsiao <sandy900825@gmail.com>
2024-02-01 11:27:50 +0800

    edit config output export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/My_New_Website",
   output: "export", // <=== enables static exports
+  basePath:
+    process.env.NODE_ENV === "production" ? "/My_New_Website" : undefined,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
 
